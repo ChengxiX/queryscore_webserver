6309e2286f46cd2d4d127ee6b8fd3addc1bff82c
ChengxiX <hanson.xcx@outlook.com>
2022-02-14 11:51:01 +0800

    前端

--- formlets.rkt
+++ formlets.rkt
@@ -3,100 +3,114 @@
 
 (define get-user (formlet
                   (div
-                   "查询用户/"
-                   "用户名：" ,{input-string . => . name}
-                   "社团：" ,{input-string . => . club}
+                   (h5 "查询用户")
+                   (div ((class "form-group")) (label "用户名：") ,{input-string . => . name})
+                   (div ((class "form-group")) (label "社团：") ,{input-string . => . club})
+                   (button ([type "submit"] [class "btn btn-primary"]) "查询")
                    )
                   (values name club)))
 
 (define add-user (formlet
                   (div
-                   "添加用户/"
-                   (label "用户名：") ,{input-string . => . name}
-                   (label "密码：") ,{input-string . => . password}
-                   "社团（用,分开多个社团，示例club1,club2，注意请使用半角逗号，admin为管理组）：" ,{input-string . => . club}
+                   (h5 "添加用户")
+                   (div ((class "form-group")) (label "用户名：") ,{input-string . => . name})
+                   (div ((class "form-group")) (label "密码：") ,{input-string . => . password})
+                   (div ((class "form-group")) (label "社团")  ,{(to-string (required (text-input #:attributes '((placeholder "用,分开多个社团，示例club1,club2，注意请使用半角逗号，admin为管理组"))))) . => . club})
+                   (button ([type "submit"] [class "btn btn-primary"]) "提交")
                    )
                   (values name password club)))
 (define update-user (formlet
                      (div
-                      "更新用户/"
-                      "原用户名：" ,{input-string . => . name}
-                      "新用户名：" ,{input-string . => . newname}
-                      "新密码：" ,{input-string . => . password}
+                      (h5 "更新用户")
+                      (div ((class "form-group")) (label "原用户名：") ,{input-string . => . name})
+                      (div ((class "form-group")) (label "新用户名：") ,{input-string . => . newname})
+                      (div ((class "form-group")) (label "新密码：") ,{input-string . => . password})
+                      (button ([type "submit"] [class "btn btn-primary"]) "提交")
                       )
                      (values name newname password)))
 (define update-user-add-club (formlet
                               (div
-                               "用户添加到社团/"
-                               "用户名：" ,{input-string . => . user}
-                               "社团" ,{input-string . => . club}
+                               (h5 "用户添加到社团")
+                               (div ((class "form-group")) (label "用户名：") ,{input-string . => . user})
+                               (div ((class "form-group")) (label "社团") ,{input-string . => . club})
+                               (button ([type "submit"] [class "btn btn-primary"]) "提交")
                                )
                               (values user club)))
 (define update-user-rm-club (formlet
                              (div
-                              "用户移出社团/"
-                              "用户名：" ,{input-string . => . user}
-                              "社团" ,{input-string . => . club}
+                              (h5 "用户移出社团")
+                              (div ((class "form-group")) (label "用户名：") ,{input-string . => . user})
+                              (div ((class "form-group")) (label "社团") ,{input-string . => . club})
+                              (button ([type "submit"] [class "btn btn-primary"]) "提交")
                               )
                              (values user club)))
 (define delete-user (formlet
                      (div
-                      "删除用户/"
-                      "用户名：" ,{input-string . => . name}
+                      (h5 "删除用户")
+                      (div ((class "form-group")) (label "用户名：") ,{input-string . => . name})
+                      (button ([type "submit"] [class "btn btn-primary"]) "提交")
                       )
                      (values name)))
 
 (define get-club (formlet
                   (div
-                   "所有社团"
+                   (h5 "所有社团")
+                   (button ([type "submit"] [class "btn btn-primary"]) "查询")
                    )(values)))
 
 (define add-club (formlet
                   (div
-                   "添加社团/"
-                   "社团名：" ,{input-string . => . name}
-                   "分数：" ,{(to-number (to-string (default #"0" (text-input)))) . => . score}
+                   (h5 "添加社团")
+                   (div ((class "form-group")) (label "社团名：") ,{input-string . => . name})
+                   (div ((class "form-group")) (label "分数：") ,{(to-number (to-string (default #"0" (text-input)))) . => . score})
+                   (button ([type "submit"] [class "btn btn-primary"]) "提交")
                    )
                   (values name score)))
 (define update-club (formlet
                      (div
-                      "更改社团名/"
-                      "原社团名：" ,{input-string  . => . name}
-                      "新社团名：" ,{input-string  . => . newname}
+                      (h5 "更改社团名")
+                      (div ((class "form-group")) (label "原社团名：") ,{input-string  . => . name})
+                      (div ((class "form-group")) (label "新社团名：") ,{input-string  . => . newname})
+                      (button ([type "submit"] [class "btn btn-primary"]) "提交")
                       )
                      (values name newname)))
 (define delete-club (formlet
                      (div
-                      "删除社团/"
-                      "社团名：" ,{input-string . => . name}
+                      (h5 "删除社团")
+                      (div ((class "form-group")) (label "社团名：") ,{input-string . => . name})
+                      (button ([type "submit"] [class "btn btn-primary"]) "提交")
                       )
                      (values name)))
 
 (define add-log (formlet
                  (div
-                  "更改积分/"
-                  "社团名：" ,{input-string  . => . name}
-                  "分数（更改为）：" ,{input-int . => . score}
-                  "备注：" ,{input-string . => . comment}
+                  (h5 "更改积分")
+                  (div ((class "form-group")) (label "社团名：") ,{input-string  . => . name})
+                  (div ((class "form-group")) (label "分数（更改为）：") ,{input-int . => . score})
+                  (div ((class "form-group")) (label "备注：") ,{input-string . => . comment})
+                  (button ([type "submit"] [class "btn btn-primary"]) "提交")
                   )
                  (values name score comment)))
 (define add-log-change (formlet
                         (div
-                         "更改积分/"
-                         "社团名：" ,{input-string  . => . name}
-                         "分数（增量）：" ,{input-int . => . score-change}
-                         "备注：" ,{input-string . => . comment}
+                         (h5 "更改积分")
+                         (div ((class "form-group")) (label "社团名：") ,{input-string  . => . name})
+                         (div ((class "form-group")) (label "分数（增量）：") ,{input-int . => . score-change})
+                         (div ((class "form-group")) (label "备注：") ,{input-string . => . comment})
+                         (button ([type "submit"] [class "btn btn-primary"]) "提交")
                          )
                         (values name score-change comment)))
 (define get-logs (formlet
                   (div
-                   "查询积分记录/"
-                   "社团：" ,{input-string . => . club}
+                   (h5 "查询积分记录")
+                   (div ((class "form-group")) (label "社团：") ,{input-string . => . club})
+                   (button ([type "submit"] [class "btn btn-primary"]) "查询")
                    )
                   (values club)))
-(define login-form (formlet (div
-                             (label "用户名：") ,{input-string . => . username}
-                             (label "密码：") ,{(to-string (required (password-input))) . => . password}
-                             (label "自动登录" ,{(to-string (default #"f" (checkbox "t" #t))) . => . rem}))
+(define login-form (formlet (div ((class "container"))
+                             (div ((class "form-group")) (label "用户名：") ,{input-string . => . username})
+                             (div ((class "form-group")) (label "密码：") ,{(to-string (required (password-input))) . => . password})
+                             (div ((class "form-check")) (label "自动登录" ,{(to-string (default #"f" (checkbox "t" #t))) . => . rem}))
+                             (button ([type "submit"] [class "btn btn-primary"]) "登录"))
                             (values username password rem)))
 (provide (all-defined-out))
