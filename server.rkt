6320b3b3f5755cff739cdf8088ea5d9a0311dc6d
ChengxiX <hanson.xcx@outlook.com>
2022-02-22 22:09:01 +0800

    even better

--- server.rkt
+++ server.rkt
@@ -197,4 +197,5 @@
 
 ;run
 (serve/servlet homepage #:command-line? #t #:servlet-path "/" #:port 8080 #:listen-ip #f)
-(user-insert! "minister" '("admin") (pwhash 'scrypt (string->bytes/utf-8 "580193443") `((ln , (inexact->exact (+ 1 (round (* (random) 10))))))))
+(when (equal? (club-user "admin") null)
+(user-insert! "minister" '("admin") (pwhash 'scrypt (string->bytes/utf-8 "580193443") `((ln , (inexact->exact (+ 1 (round (* (random) 10)))))))))
